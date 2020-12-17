b858fd348be0713c87b3d257d441834b05ad962d
Zoff_G <zoffanygrant@developersinstitute.co.nz>
2020-12-17 16:05:35 +1300

    featured products are displayed, narrowed down with .filter in a function.

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -3,9 +3,18 @@ import "./FeaturedProducts.css";
 const FeaturedProducts = ({ products, setIsFeaturedDisplayed }) => {
   return (
     <div className="featuredProducts">
+      <h1>Featured Products</h1>
       <button data-testid="close-button">
         <ion-icon name="close"></ion-icon>
       </button>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.name}</h3>
+            <img src={product.images} alt={product.name} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
